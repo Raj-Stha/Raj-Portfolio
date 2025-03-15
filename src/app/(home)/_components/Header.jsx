4171a6836f3b9664fcc56625e2508b963e74fd50
Raj-Stha <rajcrestha00@gmail.com>
2025-03-15 21:17:28 +0545

    Add :: Header

--- src/app/(home)/_components/Header.jsx
+++ src/app/(home)/_components/Header.jsx
@@ -1,13 +1,49 @@
 "use client";
 import { useTheme } from "next-themes";
+import { Moon, Sun } from "lucide-react";
+import { useState } from "react";
 
 export default function Header() {
-  const { setTheme } = useTheme();
+  const { setTheme, theme } = useTheme(); // Removed "light" argument here
+  const [isLight, setIsLight] = useState(theme === "light"); // Initialize based on current theme
 
   return (
     <>
-      <button onClick={() => setTheme("light")}>Light</button>
-      <button onClick={() => setTheme("dark")}>Dark</button>
+      <div className="flex justify-between w-full pl-4 pr-8 py-3">
+        <div className="">
+          <h1 className="text-3xl font-semibold">Rj Portfolio</h1>
+        </div>
+        <div className="flex items-center gap-5 text-lg">
+          <button className="cursor-pointer">Home</button>
+          <button className="cursor-pointer">Services</button>
+          <button className="cursor-pointer">Technology</button>
+          <button className="cursor-pointer">Projects</button>
+          <button className="cursor-pointer">Contact</button>
+          <div className="">
+            {isLight ? (
+              <div
+                className="cursor-pointer"
+                onClick={() => {
+                  setIsLight(false);
+                  setTheme("dark");
+                }}
+              >
+                <Moon />
+              </div>
+            ) : (
+              <div
+                className="cursor-pointer"
+                onClick={() => {
+                  setIsLight(true);
+                  setTheme("light");
+                }}
+              >
+                <Sun />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
